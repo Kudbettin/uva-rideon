8b22e2834e226bd59fd298a326dc25f7f5ed55b9
Jake Smith <jake@metactf.com>
2019-09-30 13:42:08 -0400

    Create requirements-build.txt for Travis CI Builds
    Created this workaround to get around problems with
    heroku imports
    https://stackoverflow.com/a/50988533/3302799

--- RideOn/settings.py
+++ RideOn/settings.py
@@ -11,7 +11,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+
+
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -131,4 +138,3 @@ LOGOUT_REDIRECT_URL = 'index'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-django_heroku.settings(locals())
