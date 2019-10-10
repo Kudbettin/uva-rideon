dd9d7eb5c30a476cb33cc78679605e77387a4164
Jake Smith <jake@metactf.com>
2019-10-10 14:58:16 -0400

    Add unit test for oauth

--- users/tests.py
+++ users/tests.py
@@ -1,17 +1,27 @@
-from django.test import TestCase
-from users.models import *
+from django.test import Client, TestCase
+from django.contrib.auth import authenticate
+from users.models import CustomUser
 
-"""
-Example unit tests to base others off of
-"""
+
 class UserTester(TestCase):
-	def setUp(self):
-		CustomUser.objects.create(email="email@email.com", username="me")
+    """
+    Social auth tests.
+    """
+    def setUp(self):
+        pass
+
+    def testCreateNewUser(self):
+        self.user = CustomUser.objects.create_user(username='testuser', email='test@example.com', password='12345')
+        self.user.set_password('12345')
+        self.user.save()
+        self.assertEqual(CustomUser.objects.all().count(), 1)
 		
-	def test_email(self):
-		users = CustomUser.objects.filter(email="email@email.com", username="me")
-		self.assertEqual(users.count(), 1)
+    def testUserLogin(self):
+        self.user = CustomUser.objects.create_user(username='testuser', email='test@example.com', password='12345')
+        self.user.set_password('12345')
+        self.user.save()
 		
-	def test_email_2(self):
-		users = CustomUser.objects.filter(email="email@email.com", username="me")
-		self.assertEqual(users.count(), 1)
+        authenticated_user = authenticate(username='testuser', password='12345')
+		
+        self.assertEqual(self.user, authenticated_user)
+		
