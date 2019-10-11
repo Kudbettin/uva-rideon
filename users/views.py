112458f8dd868064de46ee1011febb63ab9836b9
Jake Smith <jake@metactf.com>
2019-10-10 21:40:54 -0400

    Add Driver and Rider Review Models, Display on profile page

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 
 from .models import CustomUser
+from drives.models import DriverReview, RiderReview
 
 from .forms import CustomUserCreationForm
 
@@ -15,6 +16,13 @@ class RegisterView(CreateView):
 class ProfileView(generic.DetailView):
     model = CustomUser
     template_name = 'users/profile.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #context['avg_rating'] = DriverReview.objects.filter(of=kwargs['object'].id).rating
+        context['driver_avg_rating'] = 3
+        context['rider_avg_rating'] = 4
+        return context
 
 class EditProfileView(generic.DetailView):
     model = CustomUser
