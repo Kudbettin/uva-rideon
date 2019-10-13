bb9909fb99c36ff568fa478f947d84b00e671d40
Christopher Truong <christopher.vu.truong@gmail.com>
2019-10-13 05:23:11 -0400

    revert changes to master

--- RideOn/settings.py
+++ RideOn/settings.py
@@ -11,17 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj_database_url
-import dotenv
+
 import django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#load .env for db choice
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -83,8 +78,12 @@ WSGI_APPLICATION = 'RideOn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
@@ -139,7 +138,3 @@ try:
     django_heroku.settings(locals())
 except ImportError:
     found = False
-
-# workaround to avoid using SSL locally
-if os.path.isfile(dotenv_file):
-	del DATABASES['default']['OPTIONS']['sslmode']
