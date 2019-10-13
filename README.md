fd26fe8db16b3c00f4a447b95e06c393cb686f90
Kaan Katircioglu <kaankatircioglu@gmail.com>
2019-10-13 15:17:19 -0400

    creation form and few adjustments/fixes

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # RideOn
 
 A ride sharing app developed as aprt of the UVA CS3240 curriculum
+
+
+## TODO
+
+Base drive creation page
+
+----
+
+Fix the links in base drive page
+
+Get rid of base.html?
+
+
+
+
--- drives/urls.py
+++ drives/urls.py
@@ -2,7 +2,9 @@
 from django.urls import path
 from .views import *
 
+app_name = "drives"
 urlpatterns = [
   path('', RideList.as_view(), name='list'),
-  path('<pk>/', DriveView.as_view())  
+  path('<pk>/', DriveView.as_view(), name='post_details'),
+  path('new', post_new, name='post_new')  
 ]
