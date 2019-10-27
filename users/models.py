27ee039c6e7ca2a1cb2b41090fd9b89560b06559
Christopher Truong <christopher.vu.truong@gmail.com>
2019-10-27 15:20:22 -0400

    unit tests for profile edit

--- users/models.py
+++ users/models.py
@@ -12,12 +12,19 @@ def user_directory_path(instance, filename):
 
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=50, default = '')
-    gender = models.CharField(max_length=10, default = '')
+    gender = models.CharField(max_length=15, default = '')
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     about = models.TextField(max_length=1000, default = 'Write a bio to let people know about you!')
     profile_pic = models.ImageField(upload_to=user_directory_path, default='default/default_profile_pic.jpg')
     home_town = models.CharField(max_length=20, default = '')
 
+   
+
     def __str__(self):
-        return self.email
+         #get_fields gets stuff from AbstractUser that we don't want
+        local_fields = ['name', 'gender', 'phone', 'about', 'home_town']
+        field_values = []
+        for field in local_fields:
+            field_values.append(getattr(self, field, ''))
+        return ' '.join(field_values)
--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,7 @@
 from django.test import Client, TestCase
 from django.contrib.auth import authenticate
 from users.models import CustomUser
+from users.forms import CustomUserChangeForm
 from users.custom_social_auth_pipeline import allowed_email 
 
 class UserTester(TestCase):
@@ -28,4 +29,26 @@ class UserTester(TestCase):
         authenticated_user = authenticate(username='testuser', password='12345')
 		
         self.assertEqual(self.user, authenticated_user)
-		
+
+class UserEditsTester(TestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username='hacker@virginia.edu', email='hacker@virginia.edu', password='12345')
+
+    def testFormEditAllFieldsValid(self):
+        form = CustomUserChangeForm(data={'name':'John Doe' , 'gender':'Apache', 'phone':'+17037561234', 'home_town':'Cville', 'about':'Some About'})
+        self.assertTrue(form.is_valid())
+
+    def testFormEditInvalidPhone(self):
+        form = CustomUserChangeForm(data={'name':'John Doe' , 'gender':'Apache', 'phone':'+1703756123w', 'home_town':'Cville', 'about':'Some About'})
+        self.assertFalse(form.is_valid())
+
+    def testProfileEditAllFieldsValid(self):
+        local_fields = ['name', 'gender', 'phone', 'about', 'home_town']
+        correct_data = {'name':'John Doe' , 'gender':'Apache', 'phone':'+17037561234', 'home_town':'Cville', 'about':'Some About'}
+        self.form = CustomUserChangeForm(data={'name':'John Doe' , 'gender':'Apache', 'phone':'+17037561234', 'home_town':'Cville', 'about':'Some About'}, instance=self.user)
+        if self.form.is_valid():
+            self.form.save()
+
+        for field in local_fields:
+            self.assertTrue(getattr(self.user, field, '') == correct_data[field] )
